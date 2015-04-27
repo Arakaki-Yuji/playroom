cf025cfafab1aebf1fa6527e2cceaf0122f2c14c
Arakaki-Yuji <arakaki@ryukyu-i.co.jp>
2015-04-27 10:27:31 +0900

    to enable using `lein run` command.

--- clojure/programming-clj/src/programming_clj/core.clj
+++ clojure/programming-clj/src/programming_clj/core.clj
@@ -1,6 +1,12 @@
 (ns programming-clj.core)
 
+
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn -main
+  "I don't do a whole lot."
+  []
+  (foo "Yuji Arakaki"))
